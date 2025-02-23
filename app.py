ad64aa62633a88b7da68784610dbe21fbc1e7021
SamratSinghPhysicist <samratddypppis@gmail.com>
2025-02-23 18:09:08 +0530

    Trying to setup chrome on server

--- app.py
+++ app.py
@@ -81,7 +81,8 @@ def create_mega_account():
         # Use webdriver_manager with Chromium explicitly
         logging.debug("Setting up ChromeDriver with webdriver_manager...")
         service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service, options=chrome_options)
+        driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options = options)
+        # driver = webdriver.Chrome(service=service, options=chrome_options)
         wait = WebDriverWait(driver, 10)
         logging.debug("ChromeDriver initialized successfully.")
 
